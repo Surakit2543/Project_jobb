821b9ed330f13535e0136dd42e8b7eb207613826
Surakit2543 <6106021610057@fitm.kmutnb.ac.th>
2023-09-26 20:00:13 +0700

    1

---
+++ studynet_vue/src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="footer">
+        <p class="has-text-centered">Only for INET Presales department</p>
+    </footer>
+</template>
---
+++ studynet_vue/src/views/Courses.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="courses">
+        <div class="hero is-info">
+            <div class="hero-body has-text-centered">
+                <h1 class="title">Courses</h1>
+            </div>
+        </div>
+
+        <section class="section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-2">
+                        <aside class="menu">
+                            <p class="menu-label">Categories</p>
+
+                            <ul class="menu-list">
+                                <li><a class="is-active">All courses</a></li>
+                                <li><a>Programming</a></li>
+                                <li><a>Design</a></li>
+                                <li><a>UX</a></li>
+                            </ul>
+                        </aside>
+                    </div>
+
+                    <div class="column is-10">
+                        <div class="columns is-multiline">
+                            <div 
+                                class="column is-4"
+                                v-for="course in courses"
+                                v-bind:key="course.id"
+                            >
+                                
+                            </div>
+
+                            <div class="column is-12">
+                                <nav class="pagination">
+                                    <a class="pagination-previous">Previous</a>
+                                    <a class="pagination-next">Next</a>
+
+                                    <ul class="pagination-list">
+                                        <li>
+                                            <a class="pagination-link is-current">1</a>
+                                        </li>
+                                        <li>
+                                            <a class="pagination-link">2</a>
+                                        </li>
+                                        <li>
+                                            <a class="pagination-link">3</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            courses: []
+        }
+    },
+    mounted() {
+        console.log('mounted')
+
+        axios
+            .get('/api/v1/courses/')
+            .then(response => {
+                console.log(response.data)
+
+                this.courses = response.data
+            })
+    }
+}
+</script>
